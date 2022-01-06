35d2416d7118fdf5453dfd4440b18efd049ef304
SyedAhkam <smahkam57@gmail.com>
2022-01-07 04:20:43 +0530

    This should work but instead throws ANR's

--- sand_engine/src/engine.rs
+++ sand_engine/src/engine.rs
@@ -1,25 +1,48 @@
 use log::{info, debug};
 use ndk::{
-    looper::{ThreadLooper, Poll},
+    looper::{ThreadLooper, Poll, FdEvent},
     event::{InputEvent, Keycode},
 };
 
+use std::mem::MaybeUninit;
 use std::os::unix::prelude::RawFd;
 
 // First free number after ndk_glue::NDK_GLUE_LOOPER_INPUT_QUEUE_IDENT. This might be fragile.
 const CUSTOM_EVENT_IDENT: i32 = ndk_glue::NDK_GLUE_LOOPER_INPUT_QUEUE_IDENT + 1;
+const U32_SIZE: usize = std::mem::size_of::<u32>();
+
+fn create_fd_pipe() -> [RawFd; 2] {
+    let mut ends = MaybeUninit::<[RawFd; 2]>::uninit();
+
+    assert_eq!(unsafe { libc::pipe(ends.as_mut_ptr().cast()) }, 0);
+    unsafe { ends.assume_init() }
+}
 
 pub struct Engine { 
     main_looper: ThreadLooper,
-    is_running: bool
+    is_running: bool,
+    custom_event_pipe: Option<[RawFd; 2]>,
+    custom_callback_pipe: Option<[RawFd; 2]>
 } 
 
 impl Engine {
     pub fn new() -> Self {
         Self {
             main_looper: ThreadLooper::for_thread().expect("main looper not attached"),
-            is_running: true
+            is_running: true,
+            custom_event_pipe: None,
+            custom_callback_pipe: None
         }
+    }
+
+    fn handle_custom_event(&self, fd: RawFd) {
+        let mut recv = !0u32;
+        assert_eq!(
+            unsafe { libc::read(fd, &mut recv as *mut _ as *mut _, U32_SIZE) } as usize,
+            U32_SIZE
+        );
+
+        info!("Read custom event from pipe: {}", recv);
     }
 
     fn handle_event_pipe(&self) {
@@ -58,12 +81,41 @@ impl Engine {
         match ident {
             ndk_glue::NDK_GLUE_LOOPER_EVENT_PIPE_IDENT => self.handle_event_pipe(),
             ndk_glue::NDK_GLUE_LOOPER_INPUT_QUEUE_IDENT => self.handle_input(),
-            _ => {}
+            CUSTOM_EVENT_IDENT => self.handle_custom_event(fd),
+            i => panic!("Unexpected event identifier: {}", i)
         }
     }
 
-    fn setup(&self) {
-        debug!("Setup function");
+    fn setup(&mut self) {
+        // Setup file descriptors
+        self.custom_event_pipe = Some(create_fd_pipe());
+        self.custom_callback_pipe = Some(create_fd_pipe());
+
+        self.main_looper
+            .as_foreign()
+            .add_fd(
+                self.custom_event_pipe.unwrap()[0],
+                CUSTOM_EVENT_IDENT,
+                FdEvent::INPUT,
+                std::ptr::null_mut(),
+            )
+            .expect("Failed to add file descriptor to Looper");
+
+        self.main_looper
+            .as_foreign()
+            .add_fd_with_callback(self.custom_callback_pipe.unwrap()[0], FdEvent::INPUT, |fd| {
+                let mut recv = !0u32;
+
+                assert_eq!(
+                    unsafe { libc::read(fd, &mut recv as *mut _ as *mut _, U32_SIZE) } as usize,
+                    U32_SIZE
+                );
+
+                info!("Read custom event from pipe, in callback: {}", recv);
+
+                true
+            })
+            .expect("Failed to add file descriptor to Looper");
     }
 
     fn run(&mut self) {
@@ -84,6 +136,6 @@ impl Engine {
 
     pub fn start(&mut self) {
         self.setup();
-        self.run()
+        self.run();
     }
 }
